2d66de21bf3b54c97566f63cfb1f61da17e57a50
Saloni Gupta <saloni.2226csit1027@kiet.edu>
2024-01-14 16:22:39 +0530

    Initial commit

---
+++ app/src/main/java/com/amegade/mobile/splashactivity.java
@@ -0,0 +1,24 @@
+package com.amegade.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(splashactivity.this, MainActivity.class);
+                startActivity(iHome);
+            }
+        }, 3000);
+
+    }
+}
